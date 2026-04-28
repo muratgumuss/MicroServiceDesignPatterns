4afc333c7b63b76957885703e0c8bd29d3594af8
Murat Gümüş <muratgumus05@gmail.com>
2026-04-28 20:04:25 +0300

    event store commands

---
+++ EventSourcing.Api/Handlers/CreateProductCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using EventSourcing.Api.Commands;
+using EventSourcing.Api.EventStores;
+using MediatR;
+
+namespace EventSourcing.Api.Handlers
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand>
+    {
+        private readonly ProductStream _productStream;
+
+        public CreateProductCommandHandler(ProductStream productStream)
+        {
+            _productStream = productStream;
+        }
+
+        public async Task<Unit> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            _productStream.Created(request.CreateProductDto);
+
+            await _productStream.SaveAsync();
+
+            return Unit.Value;
+        }
+
+        Task IRequestHandler<CreateProductCommand>.Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            return Handle(request, cancellationToken);
+        }
+    }
+}
---
+++ EventSourcing.Api/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventSourcing.Api.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+        public int UserId { get; set; }
+    }
+}
